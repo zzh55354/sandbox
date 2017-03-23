e4634c40c11f7428acedaf7cbbab106f5b64e8b9
zzh55354 <zizh2962@colorado.edu>
2017-03-22 22:18:48 -0700

    Update Stack_Operate.c

--- exercise/Stacl_Operate/Stack_Operate.c
+++ exercise/Stacl_Operate/Stack_Operate.c
@@ -5,49 +5,70 @@
 typedef struct {
     int *top;
     int *base;
+    int *move;
+    int *min;
     int Stack_Size;
-} Stack;
-
-
-void Stack_Create(Stack* S) {
+} MinStack;
+#define sizeIncrease 2
+/** initialize your data structure here. */
+MinStack* minStackCreate(MinStack *S,int maxSize) {
     S->base = (int *)malloc(maxSize*sizeof(int));
     S->top = S->base;
     S->Stack_Size = maxSize;
+    return S;
 }
 
-void Stack_Push(Stack* S, int x) {
-	if((S->top-S->base) >= S->Stack_Size)
+void minStackPush(MinStack* S, int x) {
+    if((S->top-S->base) >= S->Stack_Size)
 	{
-		printf("%s","overflow");
-		return;
+		S->base = (int *)realloc(S->base,(S->Stack_Size+sizeIncrease)*sizeof(int));
+		S->top = S->base+S->Stack_Size;
+		S->Stack_Size = S->Stack_Size+sizeIncrease;
 	}
 	
 	*(S->top) = x;
 	S->top++;
 }
 
+void minStackPop(MinStack* S) {
+    if((S->top == S->base))
+	{
+		return 0;
+	}
+    S->top--;
+}
 
-void Stack_Pop(Stack* S) {
-		if((S->top == S->base)){
-			printf("%s","is empty");
-			return;
+int minStackTop(MinStack* S) {
+    if(S->top == S->base){
+        return 0;
+    }
+    return (*(S->top-1)); 
+}
+
+int minStackGetMin(MinStack* S) {
+    S->move = S->base;
+    S->min = S->base;
+	while(S->move <= S->top-1){
+		if(*(S->min) > *(S->move)){
+			S->min = S->move;
 		}
-		S->top--;
+		S->move++;
+	}
+	return (*(S->min));
 }
 
-int Stack_Top(Stack* S) {
-  return(*(S->top));  
+void minStackFree(MinStack* S) {
+    free(S->base);
+	S->base = NULL;
+	S->top = NULL;
 }
 
-
-
- int main()
-{
-	Stack* obj = (Stack *)malloc(sizeof(Stack));
-	Stack_Create(obj);
-	Stack_Push(obj,3);
-	int p = Stack_Top(obj);
-	printf ("%s", "Top element is: ");
-	printf ("%d", p);
-	return(0);
-}
+/**
+ * Your MinStack struct will be instantiated and called as such:
+ * struct MinStack* obj = minStackCreate(maxSize);
+ * minStackPush(obj, x);
+ * minStackPop(obj);
+ * int param_3 = minStackTop(obj);
+ * int param_4 = minStackGetMin(obj);
+ * minStackFree(obj);
+ */
