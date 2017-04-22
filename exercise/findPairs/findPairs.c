d7b4ac0b68b72c5a2386f90de143325ab0645b65
Ziheng Zhu <zizh2962@colorado.edu>
2017-04-21 21:56:50 -0600

    Leetcode accepted version

--- exercise/findPairs/findPairs.c
+++ exercise/findPairs/findPairs.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 
+int get_min( int a, int b);
+int is_save(int *c, int d, int pos);
 
 int findPairs(int* nums, int numsSize, int k) {
+	if(k < 0){
+		return 0;
+	}	
     int answer = 0;
     int p = 0;
     int q = 0;
-    int min_array[5000];
+    int min_array[10000];
     while(p < numsSize-1) {
 		q = p + 1;
 		while(q < numsSize) {
 			if(nums[p]-nums[q] == k || nums[q]-nums[p] == k){
 				int min = get_min(nums[p], nums[q]);
-				if(is_save(min_array, min) == 1) { 
+				if(is_save(min_array, min, answer) == 1) { 
 					answer++;
 				}	
 			}
@@ -30,22 +35,20 @@ int get_min( int a, int b) {
 		return b;	
 }	
 
-int is_save(int *c, int d) {
-	int i = 0;
-	while(c[i]) {
+int is_save(int *c, int d, int pos) {
+	for(int i = 0; i < pos; i++) {
 		if(c[i] == d) {
 			return 0;
-		}
-		i++;		
+		}		
 	}
-	c[i] = d;
+	c[pos] = d;
 	return 1;
 }
 
 int main() {
 	
-int a[5]={1,1,1,1,1}; 	
-int b = findPairs(a,5,0);
+int a[5]={1,2,3,4,5}; 	
+int b = findPairs(a,5,1);
 
 printf("%d",b);
 
