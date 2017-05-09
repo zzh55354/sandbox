6644bae3d4dd9c2cff30ff7c167edb4bb0783ba6
Ziheng Zhu <zizh2962@colorado.edu>
2017-05-09 00:30:05 -0600

    NULL point access

--- exercise/findTilt/findTilt.c
+++ exercise/findTilt/findTilt.c
@@ -7,49 +7,55 @@ typedef struct TreeNode {
     struct TreeNode *right;
 }TreeNode;
 
-int node_substract(struct TreeNode* Node) {
-	if(Node->left == NULL && Node->right == NULL) {
-        return 0;
-    }
-    else if(Node->left == NULL) {
-        return Node->right->val;
-    }
-    else if(Node->right == NULL) {
-        return Node->left->val;
-    }
-    if(Node->left->val >= Node->right->val) {
-        return Node->left->val - Node->right->val;
-    }
-    else if(Node->left->val < Node->right->val) {
-        return Node->right->val- Node->left->val;
-    }
-	return 0;	
-}
- 
+int sum(struct TreeNode* Node) {
+	if (Node == NULL) {
+		return 0;
+	}
+	else 
+		return Node->val + sum(Node->right) + sum(Node->left);
+}		
+
+int nodetilt(struct TreeNode *Node) {
+	int sum_right = sum(Node->right);
+	int sum_left = sum(Node->left);
+	if(sum_right >= sum_left) {
+		return sum_right - sum_left;
+	}
+	else 
+		return sum_left - sum_right;
+} 
+
+
 int findTilt(struct TreeNode* root) {
     if(root == NULL) {
         return 0;
     }
     else 
-        return node_substract(root)+findTilt(root->left)+findTilt(root->right);
+        return nodetilt(root)+nodetilt(root->left)+nodetilt(root->right);
 }
 int main(){
-	TreeNode *a,*b,*c,*d,*e;
+	TreeNode *a,*b;//*c,*d,*e,*g;
 	a = (TreeNode *)malloc(sizeof(TreeNode));
 	b = (TreeNode *)malloc(sizeof(TreeNode));
-	c = (TreeNode *)malloc(sizeof(TreeNode));
-	d = (TreeNode *)malloc(sizeof(TreeNode));
-	e = (TreeNode *)malloc(sizeof(TreeNode));
+	//c = (TreeNode *)malloc(sizeof(TreeNode));
+	//d = (TreeNode *)malloc(sizeof(TreeNode));
+	//e = (TreeNode *)malloc(sizeof(TreeNode));
+	
+	//g = (TreeNode *)malloc(sizeof(TreeNode));
 	a->left = b;
-	a->right = c;
-	b->right = d;
-	c->left = e;
+	/*a->right = c;
+	b->right = e;
+	
+	b->left = d;
+	c->right = g;*/
 	a->val = 1;
 	b->val = 2;
-	c->val = 3;
-	d->val = 4;
-	e->val = 5;
-	int tilt = findTilt(a);
-	printf("%d", tilt);
+	//c->val = 3;
+	//d->val = 4;
+	//e->val = 5;
+	
+	//g->val = 7;
+	int findtilt = findTilt(a);
+	printf("%d", findtilt);
 	return 0;
 }	
