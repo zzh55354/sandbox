bfba0cfb73a5c6107162d9a5ae00c0e86b6646f5
Ziheng Zhu <zizh2962@colorado.edu>
2017-05-13 00:03:21 -0600

    leetcode accepted version

--- exercise/findTilt/findTilt.c
+++ exercise/findTilt/findTilt.c
@@ -16,6 +16,7 @@ int sum(struct TreeNode* Node) {
 }		
 
 int nodetilt(struct TreeNode *Node) {
+
 	int sum_right = sum(Node->right);
 	int sum_left = sum(Node->left);
 	if(sum_right >= sum_left) {
@@ -31,30 +32,26 @@ int findTilt(struct TreeNode* root) {
         return 0;
     }
     else 
-        return nodetilt(root)+nodetilt(root->left)+nodetilt(root->right);
+        return nodetilt(root)+findTilt(root->left)+findTilt(root->right);
 }
 int main(){
-	TreeNode *a,*b;//*c,*d,*e,*g;
+	TreeNode *a,*b,*c,*d,*e;
 	a = (TreeNode *)malloc(sizeof(TreeNode));
 	b = (TreeNode *)malloc(sizeof(TreeNode));
-	//c = (TreeNode *)malloc(sizeof(TreeNode));
-	//d = (TreeNode *)malloc(sizeof(TreeNode));
-	//e = (TreeNode *)malloc(sizeof(TreeNode));
-	
-	//g = (TreeNode *)malloc(sizeof(TreeNode));
+	c = (TreeNode *)malloc(sizeof(TreeNode));
+	d = (TreeNode *)malloc(sizeof(TreeNode));
+	e = (TreeNode *)malloc(sizeof(TreeNode));
 	a->left = b;
-	/*a->right = c;
-	b->right = e;
+	b->left = c;
+	c->left = d;
+	d->left = e;
 	
-	b->left = d;
-	c->right = g;*/
 	a->val = 1;
 	b->val = 2;
-	//c->val = 3;
-	//d->val = 4;
-	//e->val = 5;
+	c->val = 3;
+	d->val = 4;
+	e->val = 5;
 	
-	//g->val = 7;
 	int findtilt = findTilt(a);
 	printf("%d", findtilt);
 	return 0;
